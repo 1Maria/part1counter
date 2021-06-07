45c8eee3dc3bfc58946cf4fbcd9c5164fe88ada2
full_name <email_address>
2021-06-07 09:27:30 -0400

    Define event handler function directly in the value assignment of the onClick-attribute

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [ counter, setCounter ] = useState(0);
-  
-  const handleClick = () => {
-    console.log('clicked');
-  }
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>
-        plus
+      <button onClick={() => console.log('clicked')}>
+       plus
       </button>  
     </div>
   );
